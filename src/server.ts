42b65cf03cacd7f0c4cb1bd80b784532a0922c05
DevVitorSilva <joao@joaovitor.com>
2023-09-03 09:09:59 -0300

    creating server.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from "express";
 import { router } from "./routes/contactRoutes";
+import { config } from "dotenv";
+import { db } from "./db/db";
 
+config()
 const app = express();
-const PORT: number = 9001
+const PORT = process.env.PORT || 9001
+const URI = process.env.URI
 
 app.use('/api/v1', express.json(), router)
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`)
+    db(URI as string)
 })
